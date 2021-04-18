4e6897e05cacd99f1e433fc5521751e6a5eb149a
James Cruz <cruz.james99@gmail.com>
2021-04-18 17:12:42 +0800

    Added subject code template

--- src/main/java/NewMain.java
+++ src/main/java/NewMain.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Cruz, James Laurence A.
+Dela Cruz, Rica V.
+Lenizo, Jackilyn O.
+Paz, Kristel Erica D.
+Sacayanan, Christian John P.
+BS CpE 2-3 - Software Design
+
+Week 5 - Laboratory Exercise
+Group yourselves into 5 members. Create a POS program that apply the concepts of
+OOP (Inheritance, Polymorphism, Abstraction, and Encapsulation), with the following
+conditions:
+    - Create a main class, 1 superclass and atleast 2 sub-class
+    - Simulate a purchase of a customer
  */
 
 /**
@@ -10,9 +20,6 @@
  */
 public class NewMain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("softlab");
         System.out.println("Henlo hehe");
